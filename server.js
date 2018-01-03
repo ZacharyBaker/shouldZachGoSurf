fe3a3458dc167a38dbd809cb26261aad99755321
zach <zach.r.baker1@gmail.com>
2018-01-03 08:22:37 -0800

    go surf

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 5000
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Should zach go surf? yes');
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
 });
